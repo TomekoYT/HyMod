4fbef903a41750c2fe1749faf283e8d3f3a4226b
TomekoYT <tomekoyt@interia.pl>
2026-05-25 00:23:01 +0200

    fix oneconfig gui freezing the game when alt-tabbed

--- src/main/java/tomeko/hymod/HyMod.java
+++ src/main/java/tomeko/hymod/HyMod.java
@@ -4,6 +4,7 @@ import cc.polyfrost.oneconfig.events.EventManager;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import tomeko.hymod.config.HyModConfig;
+import tomeko.hymod.gui.CloseInactiveConfigScreen;
 import tomeko.hymod.utils.Constants;
 
 @Mod(modid = Constants.MOD_ID, name = Constants.MOD_NAME, version = Constants.MOD_VERSION, modLanguageAdapter = "cc.polyfrost.oneconfig.utils.KotlinLanguageAdapter")
@@ -14,5 +15,7 @@ public class HyMod {
     public void onInit(FMLInitializationEvent event) {
         config = new HyModConfig();
         EventManager.INSTANCE.register(this);
+
+        CloseInactiveConfigScreen.register();
     }
 }
---
+++ src/main/java/tomeko/hymod/gui/CloseInactiveConfigScreen.java
@@ -0,0 +1,25 @@
+package tomeko.hymod.gui;
+
+//? if = 1.8.9 {
+
+import org.lwjgl.opengl.Display;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent.ClientTickEvent;
+
+public class CloseInactiveConfigScreen {
+    private static final Minecraft mc = Minecraft.getMinecraft();
+
+    public static void register() {
+        MinecraftForge.EVENT_BUS.register(new CloseInactiveConfigScreen());
+    }
+
+    @SubscribeEvent
+    public void onTick(ClientTickEvent event) {
+        if (Display.isActive() || mc.currentScreen == null || !mc.currentScreen.getClass().getName().contains("oneconfig"))
+            return;
+        mc.displayGuiScreen(null);
+    }
+}
+//?}
