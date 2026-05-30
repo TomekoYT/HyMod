53275df24f15f1d14acbcbb261b49759bc3cafac
TomekoYT <tomekoyt@interia.pl>
2026-05-30 21:40:15 +0200

    port DangerousTauntWaypoint to 1.21.9+

--- src/main/java/tomeko/hymod/chat/DangerousTauntWaypoint.java
+++ src/main/java/tomeko/hymod/chat/DangerousTauntWaypoint.java
@@ -1,33 +1,77 @@
 package tomeko.hymod.chat;
 
 import net.minecraft.client.Minecraft;
+//? if = 1.8.9 {
 import net.minecraft.client.multiplayer.WorldClient;
+import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.BlockPos;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+//?} else {
+/*import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+*///?}
 import tomeko.hymod.config.HyModConfig;
 import tomeko.hymod.utils.HypixelPackets;
 import tomeko.hymod.utils.StringFormatting;
 import tomeko.hymod.utils.Waypoint;
 import tomeko.hymod.utils.WaypointRenderer;
 
-import java.awt.*;
+import java.awt.Color;
 
 public class DangerousTauntWaypoint {
+    private static final Block AIR =
+            //? if = 1.8.9 {
+            Blocks.air;
+            //?} else {
+            /*Blocks.AIR;
+    *///?}
+
     private static final int seconds = 15;
 
     public static void register() {
+        //? if = 1.8.9 {
         MinecraftForge.EVENT_BUS.register(new DangerousTauntWaypoint());
+        //?} else {
+        /*ClientReceiveMessageEvents.GAME.register(DangerousTauntWaypoint::onChat);
+        *///?}
     }
 
+    //? if = 1.8.9 {
     @SubscribeEvent
-    public void onChat(ClientChatReceivedEvent event) {
-        if (event.type == 2 || event.message == null || !HyModConfig.dangerousTauntWaypointEnabled || !HypixelPackets.inArcade)
+    //?} else {
+    /*static
+            *///?}
+    public void onChat(
+            //? if = 1.8.9 {
+            ClientChatReceivedEvent event
+            //?} else {
+            /*Component component,
+            boolean fromActionBar
+            *///?}
+    ) {
+        //? if = 1.8.9 {
+        if (event.type == 2 || event.message == null)
+        //?} else {
+        /*if (fromActionBar || component == null)
+            *///?}
             return;
 
-        String message = StringFormatting.removeFormatting(event.message.getUnformattedText());
+        if (!HyModConfig.dangerousTauntWaypointEnabled || !HypixelPackets.inArcade) return;
+
+        String message = StringFormatting.removeFormatting(
+                //? if = 1.8.9 {
+                event.message.getUnformattedText()
+                //?} else {
+                /*component.getString()
+                *///?}
+        );
         String[] words = message.split(" ");
         if (words.length != 12
                 || (!words[0].equals("A") && !words[0].equals("An"))
@@ -86,17 +130,29 @@ public class DangerousTauntWaypoint {
     }
 
     private static BlockPos getPos(int x, int z) {
+        //? if = 1.8.9 {
         WorldClient world = Minecraft.getMinecraft().theWorld;
+        //?} else {
+        /*ClientLevel world = Minecraft.getInstance().level;
+        *///?}
 
         BlockPos pos = new BlockPos(x, 0, z);
-        while (world.getBlockState(pos).getBlock() == Blocks.air) {
-            pos = pos.up();
+        while (world.getBlockState(pos).getBlock() == AIR) {
+            pos = oneHigher(pos);
         }
 
-        while (world.getBlockState(pos).getBlock() != Blocks.air) {
-            pos = pos.up();
+        while (world.getBlockState(pos).getBlock() != AIR) {
+            pos = oneHigher(pos);
         }
 
         return pos;
+    }
+
+    private static BlockPos oneHigher(BlockPos pos) {
+        //? if = 1.8.9 {
+        return pos.up();
+        //?} else {
+        /*return pos.above();
+        *///?}
     }
 }
